3c02043045a11543515c578108ce61714840d6c0
kaionobre <kaionobrega00@gmail.com>
2025-03-10 12:22:06 -0300

    ADD: Adicionando o arquivo requirements.txt

--- backend/apps/empresa/api/router.py
+++ backend/apps/empresa/api/router.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from apps.empresa.api.viewsets import EmpresaViewSet
 
 router = DefaultRouter()
-router.register(r'vagas', EmpresaViewSet)
+router.register(r'empresas', EmpresaViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
