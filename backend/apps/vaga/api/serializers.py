277bc7aa5d716f1fcf3febf3436f4cd3ec6076ef
kaionobre <kaionobrega00@gmail.com>
2025-04-01 12:16:01 -0300

    ATT: Modificando a viewset do APP Vaga

--- backend/apps/vaga/api/serializers.py
+++ backend/apps/vaga/api/serializers.py
@@ -4,4 +4,11 @@ from apps.vaga.models import Vaga
 class VagaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Vaga
-        fields = '__all__'  
+        fields = '__all__'
+        extra_kwargs = {'recrutador': {'read_only': True}}  # Recrutador não pode ser enviado no JSON
+
+    def create(self, validated_data):
+        request = self.context['request']  # Pega a requisição
+        recrutador = request.user.recrutador  # Obtém o recrutador vinculado ao usuário logado
+        validated_data['recrutador'] = recrutador  # Define automaticamente o recrutador
+        return super().create(validated_data)
--- backend/apps/vaga/api/viewsets.py
+++ backend/apps/vaga/api/viewsets.py
@@ -1,7 +1,14 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from apps.vaga.models import Vaga
+from apps.vaga.api.permissions import IsRecrutador, IsOwnerOrReadOnly
 from apps.vaga.api.serializers import VagaSerializer
 
 class VagaViewSet(viewsets.ModelViewSet):
     queryset = Vaga.objects.all()
     serializer_class = VagaSerializer
+    permission_classes = [IsAuthenticated, IsRecrutador, IsOwnerOrReadOnly]  # Apenas usuários autenticados podem acessar
+
+    def perform_create(self, serializer):
+        # Associa a vaga ao recrutador do usuário logado
+        serializer.save(recrutador=self.request.user.recrutador)
